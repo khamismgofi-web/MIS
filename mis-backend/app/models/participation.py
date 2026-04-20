7cfece4aec6fd07f564dd20c1ba6b86688c78f5e
khamismgofi-web <khamismgofi18@gmail.com>
2026-04-20 13:54:03 +0300

    chore: adding new features

--- mis-backend/app/models/participation.py
+++ mis-backend/app/models/participation.py
@@ -1,1 +1,25 @@
 # participation (user -> project join table)
+from sqlalchemy import Mapped,mapped_column,relationship
+from sqlalchemy import ForeignKey,DateTime,Enum,func,UniqueConstraint
+from app.core.database import Base
+import enum,uuid
+
+class ParticipationRole(str,enum,Enum):
+    LEAD = "Lead"
+    MEMBER = "Member"
+    OBSERVER = "Observer"
+
+class Participation(Base):
+    __tableName__ = "Participation"
+    __table_agre__ = (UniqueConstraint('user_id','project_id',name = "uq_user_ptoject"),)
+
+
+id : Mapped[uuid.UUID] = mapped_column(primary_key= True,default =uuid.uuid4)
+user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('user.id'),nullable = False)
+role: Mapped[ParticipationRole] = mapped_column(Enum(ParticipationRole),default = ParticipationRole.MEMBER)
+join_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True),server_default=func.now())
+
+
+#Relationship
+User:Mapped['User'] = relationship(back_populates = 'participation')
+project:Mapped['Project'] = relationship(back_populates='participation')
