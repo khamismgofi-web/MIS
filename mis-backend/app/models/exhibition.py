063ab852b5bef7fc87b0d726b188525861588f87
khamismgofi-web <khamismgofi18@gmail.com>
2026-04-20 17:03:47 +0300

    feat:add new features

--- mis-backend/app/models/exhibition.py
+++ mis-backend/app/models/exhibition.py
@@ -16,4 +16,12 @@ class Exhibition(Base):
 id: Mapped[uuid.UUId] = mapped_column(primary_key=True,default=uuid.uuid4)
 name:Mapped[String] = mapped_column(String(255),nullable=False)
 description:Mapped[str|None] = mapped_column(Text,nullable=True)
-venue:Mapped[str|None] = mapped_column(String(300))
+venue:Mapped[str|None] = mapped_column(String(300),nullable=True)
+event_date:Mapped[Date] = mapped_column(Date,nullable=False)
+status:Mapped[ExhibitionStatus] = mapped_column(Enum(ExhibitionStatus),default=ExhibitionStatus.UPCOMING)
+created_at:Mapped[DateTime] = mapped_column(DateTime(timezone=True),server_default=func.now())
+
+
+#Relatiionship
+entries:Mapped[list['ShowEntry']] = relationship(back_populates = 'exhibition')
+reports:Maped[lsit['Report']] = relationship(back_populates='exhibition')
