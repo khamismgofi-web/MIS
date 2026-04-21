23935aae9f5439155c3e34ee8d72f57c870137f5
khamismgofi-web <khamismgofi18@gmail.com>
2026-04-21 16:38:22 +0300

    feat: add pydantic settings

--- mis-backend/app/core/config.py
+++ mis-backend/app/core/config.py
@@ -1,1 +1,13 @@
 #Loads .env settings via pydantic settings
+from pydantic_settings import BaseSettings
+
+class Setting(BaseSettings):
+    DATABASE_URL : str
+    SECRET_KEY : str
+    ALGORITHM : str="HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES : int=60
+    APP_NAME:  str="MIS Platform"
+    DEBUG: bool=False
+
+    model_config={"env_file":".env"}
+Setting=Setting()
