73a670b78aeff344f43f4f9ec8f6f46e03306244
khamismgofi-web <khamismgofi18@gmail.com>
2026-04-21 15:10:58 +0300

    feat:add some features

--- mis-backend/app/models/report.py
+++ mis-backend/app/models/report.py
@@ -16,3 +16,11 @@ id:Mapped[uuid.UUId] = mapped_column(primary_key=True,default=uuid.uuid4)
 report_type:Mapped[ReportType] = mapped_column(Enum(ReportType),nullable=False)
 tittle:Mapped[str] = mapped_column(String(300),nullable=False)
 content:Mapped[str] = mapped_column(Text,nullable=False)
+exhibition_id:Mapped[uuid.UUID|None] = mapped_column(ForeignKey('User.id'),nullable=True)
+generated_by_id:Mapped[uuid.UUID] = mapped_column(ForeignKey('User.id'),nullable=False)
+generated_at:Mapped[DateTime] = mapped_column(DateTime(timezone=True),server_default=func.now())
+
+
+#Relationship
+exhibition:Mapped['Exhibition|None'] = relationship(back_populates='reports')
+generated_by:Mapped['User'] = relationship(back_populates='reports')
