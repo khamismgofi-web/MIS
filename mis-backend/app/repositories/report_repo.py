7095237b84f349796e937580cb55c792c6d071c8
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-03 12:36:48 +0300

    feat:add new features

--- mis-backend/app/repositories/report_repo.py
+++ mis-backend/app/repositories/report_repo.py
@@ -1,1 +1,6 @@
-#Report generation querries
+#Report generation querriesfrom sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from app.models.report import report
+from app.schemas.report import reportCreate
+import uuid
