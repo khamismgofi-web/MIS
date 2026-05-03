1fd162f6aba23d6ff7867bd19390c6d1e73ef9cd
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-03 12:33:34 +0300

    feat:add new features

--- mis-backend/app/repositories/exhibition_repo.py
+++ mis-backend/app/repositories/exhibition_repo.py
@@ -1,1 +1,7 @@
 #Exhibition DB querries
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from app.models.exhibition import exhibition
+from app.schemas.exhibition import exhibitionCreate
+import uuid
