e638a3f392fb69c2a3f99f6c3285f1e57733343b
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-03 12:40:50 +0300

    feat: add new features

--- mis-backend/app/repositories/user_repo.py
+++ mis-backend/app/repositories/user_repo.py
@@ -1,1 +1,7 @@
 #user DB Querries
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from app.models.user import UserRole
+from app.schemas.users import UserCreate
+import uuid
