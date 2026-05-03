85341d6bfc657885d7597beb6a7f5b2f85c30be7
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-03 12:56:14 +0300

    feat:add new features

--- mis-backend/routers/project.py
+++ mis-backend/routers/project.py
@@ -1,1 +1,7 @@
-#Project CRUD endpoints
+#Project CRUD endpointsfrom fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.dependencies import get_db, get_current_user
+from app.services.project_services import ProjectService
+from app.schemas.projects import ProjectCreate, ProjectRead
+from app.models.user import User
+import uuid
