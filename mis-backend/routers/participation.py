a123ccf0ec847fa7082a8faf6c0e7fd4a9eb30ae
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-03 12:45:21 +0300

    fix: fixing some bugs

--- mis-backend/routers/participation.py
+++ mis-backend/routers/participation.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.dependencies import get_db, get_current_user
-from app.services.participation_service import ParticipationService
+from app.services.participation_services import ParticipationService
 from app.schemas.participation import ParticipationCreate, ParticipationRead
 from app.models.user import User
 import uuid
