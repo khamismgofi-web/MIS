8962661f14701703891af1f550b4bd57f32de602
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-03 12:35:34 +0300

    feat: add new features

--- mis-backend/app/repositories/project_repo.py
+++ mis-backend/app/repositories/project_repo.py
@@ -1,1 +1,7 @@
 #Project DB querries
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from app.models.project import project
+from app.schemas.projects import projectCreate
+import uuid
