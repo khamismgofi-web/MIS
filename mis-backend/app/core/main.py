e42b3ffb99affc2b3a6aa5967401522de63bfe75
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-28 18:18:31 +0300

    feat:create table in  Neon database

--- mis-backend/app/core/main.py
+++ mis-backend/app/core/main.py
@@ -1,2 +1,5 @@
-from app.core.database import database
+from app.core.database import get_engine,Base
 import models
+print("Connecting to Neon and creating tables")
+Base.metadata.create_all(bind=get_engine)
+print("Tables created successfully")
